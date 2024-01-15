980cd377a076a07a7311f325320bc17e13797096
scaracciolo <santiago.caracciolo@gmail.com>
2024-01-15 13:02:18 -0300

    add check

--- src/isotropic_remesher/utils.py
+++ src/isotropic_remesher/utils.py
@@ -2,7 +2,8 @@ import numpy as np
 from open3d.utility import Vector3dVector 
 from open3d.geometry import PointCloud 
 
-def compute_len_stats(vertices:Vector3dVector):
+def compute_len_stats(vertices:np.ndarray):
+    vertices = Vector3dVector(vertices) if isinstance(vertices, np.ndarray) else vertices
     pcd = PointCloud(vertices)
     d = np.asarray(pcd.compute_nearest_neighbor_distance())
     return d.min(), d.mean(), d.max()
